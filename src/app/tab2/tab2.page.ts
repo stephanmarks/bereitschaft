f29f13b4e8430d316b3d2a9785ed66f2517dfd9f
Stephan Marks <stephan@marks-home.de>
2019-10-09 20:13:35 +0200

    Kalenderanzeige

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -9,13 +9,20 @@ import { Component, OnInit} from '@angular/core';
 })
 export class Tab2Page implements OnInit  {
 
-  sparten: Array<any>;
   bereitschaften: Array<any>;
+  bereitschaftsDates: Array<any>;
 
   constructor(private dataService: DataService, private router: Router) {}
 
-  ngOnInit(): void {
 
+  ngOnInit(): void {
+    this.bereitschaften = this.dataService.getBereitschaften();
+    // Nur das Datum aus Array Bereitschaften in Array myFilter schreiben
+    const myFilter = this.bereitschaften.map(filter => filter.date);
+    // Nur eindeutige Daten ins Objekt myReduced schreiben
+    const myReduced = new Set(myFilter);
+    // Neues Array aus dem Objekt myReduced erzeugen
+    this.bereitschaftsDates = [...myReduced];
   }
 
 
